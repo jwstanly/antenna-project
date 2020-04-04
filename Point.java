d6a3f1296c2095aeceaf01ff0ff4fd3de8824cc2
root <willway@outlook.com>
2020-04-03 21:50:28 -0400

    Rename files to conform to convention - Will McCoy

---
+++ Point.java
@@ -0,0 +1,23 @@
+public class Point{
+
+	private double x;
+	private double y;
+	private double z;
+	private double r;
+	private double nx;
+	private double ny;
+	private double nz;
+
+	public point(double x, double y, double z, double r, double nx, double ny, double nz){
+		this.x=x;
+		this.y=y;
+		this.z=z;
+		this.r=r;
+		this.nx=nx;
+		this.ny=ny;
+		this.nz=nz;
+	}
+
+	// I did not feel like writing getters and setters LMAOO
+
+}
