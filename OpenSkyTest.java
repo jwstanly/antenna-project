e529a67957c2922a8913ab05ad15a8733aa0addb
Will McCoy <willway@outlook.com>
2020-04-06 12:56:50 -0400

    Now can connect to the OpenSky API

---
+++ OpenSkyTest.java
@@ -0,0 +1,25 @@
+import org.opensky.api.OpenSkyApi;
+import org.opensky.model.OpenSkyStates;
+
+import java.io.IOException;
+
+public class OpenSkyTest {
+	public static void main(String args[]) throws IOException {
+		OpenSkyApi api = new OpenSkyApi("GTUsers", "#!jsawk2020");
+		
+		
+		//Lakeland: 28.01.40N, 82.01.47W
+		//Titusville: 28.36.39N, 80.49.41W
+		
+		OpenSkyStates os = api.getStates(0, null, 
+			new OpenSkyApi.BoundingBox(
+				28 + 01 / 60. + 40 / 3600.,
+				28 + 36 / 60. + 39 / 3600.,
+				-(82 + 01 / 60. + 47 / 3600.),
+				-(80 + 49 / 60. + 41 / 3600.)
+			)
+		);
+		
+		System.out.println(os.getStates().size());
+	}
+}
